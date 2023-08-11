55c7e6216bc983f078009e2c0a082a9a66ddee12
dfldylan <dfldylan@qq.com>
2023-08-11 16:26:18 +0800

    feat: add scale in weighted_mse loss, add density_loss_pbf

--- utils/tools/losses.py
+++ utils/tools/losses.py
@@ -9,15 +9,16 @@ def get_window_func(typ, fac=1.0, **kwargs):
     if typ == "poly6":
 
         def func(q):
-            return fac * tf.clip_by_value((1 - q)**3, 0, 1)
-    elif typ == "cubic":
+            return fac * tf.clip_by_value((1 - q) ** 3, 0, 1)
+    elif typ == "cubic":  # fac = 6.0 / (np.pi * h**3)
 
         def func(q):
             q_sqrt = tf.sqrt(q)
-            return fac * 4 / 3 * tf.compat.v1.where(
-                q <= 1,
-                tf.compat.v1.where(q_sqrt <= 0.5, 6 * (q_sqrt**3 - q) + 1, 2 *
-                                   (1 - q_sqrt)**3), tf.zeros_like(q_sqrt))
+            return fac * 4 / 3 * tf.compat.v1.where(q <= 1,
+                                                    tf.compat.v1.where(q_sqrt <= 0.5,
+                                                                       6 * (q_sqrt ** 3 - q) + 1,
+                                                                       2 * (1 - q_sqrt) ** 3),
+                                                    tf.zeros_like(q_sqrt))
     elif typ == "linear":
 
         def func(q):
@@ -30,13 +31,14 @@ def get_window_func(typ, fac=1.0, **kwargs):
             return fac * (1 - 2 * q_sqrt + q)
     elif typ == "cubic_grad":
 
-        def func(q):
-            #return tf.where(q <= 1, 1.0, 0.0)
+        def func(q):  # fac = 6.0 / (np.pi * h**3) (r/(rl*h))
+            # return tf.where(q <= 1, 1.0, 0.0)
             q_sqrt = tf.sqrt(q)
-            return fac * 4 / 3 * tf.compat.v1.where(
-                q <= 1,
-                tf.compat.v1.where(q_sqrt <= 0.5, 18 * q - 12 * q_sqrt, -6 *
-                                   (1 - q_sqrt)**2), tf.zeros_like(q_sqrt))
+            return fac * 4 / 3 * tf.compat.v1.where(q <= 1,
+                                                    tf.compat.v1.where(q_sqrt <= 0.5,
+                                                                       18 * q - 12 * q_sqrt,
+                                                                       -6 * (1 - q_sqrt) ** 2),
+                                                    tf.zeros_like(q_sqrt))
     elif typ is None:
         func = None
     else:
@@ -51,7 +53,7 @@ def get_loss(typ, fac=1.0, **kwargs):
             pre_f = tf.exp(-kwargs.get("pre_scale", 0.0) *
                            tf.cast(kw.get("pre_steps"), tf.float32))
             diff = (tf.reduce_sum(
-                (target - pred)**2, axis=-1) + 1e-9)**kwargs.get("gamma", 0.5)
+                (target - pred) ** 2, axis=-1) + 1e-9) ** kwargs.get("gamma", 0.5)
             return fac * tf.reduce_mean(pre_f * diff)
 
         return f
@@ -62,8 +64,9 @@ def get_loss(typ, fac=1.0, **kwargs):
                            tf.cast(kw.get("pre_steps"), tf.float32))
             importance = tf.exp(-kwargs.get("neighbor_scale", 1.0) *
                                 kw.get("num_fluid_neighbors"))
+            scale = -kwargs.get("scale", 1.0)
             diff = (tf.reduce_sum(
-                (target - pred)**2, axis=-1) + 1e-9)**kwargs.get("gamma", 0.5)
+                ((target - pred) * scale) ** 2, axis=-1) + 1e-9)**kwargs.get("gamma", 0.5)
             return fac * tf.reduce_mean(pre_f * importance * diff)
 
         return f
@@ -76,8 +79,8 @@ def get_loss(typ, fac=1.0, **kwargs):
             inp = kw.get("input")[0]
             prev = kw.get("target_prev")
             diff = (tf.reduce_sum(
-                ((target - prev) - (pred - inp))**2, axis=-1) +
-                    1e-9)**kwargs.get("gamma", 0.5)
+                ((target - prev) - (pred - inp)) ** 2, axis=-1) +
+                    1e-9) ** kwargs.get("gamma", 0.5)
             return fac * tf.reduce_mean(diff)
 
         return f
@@ -89,8 +92,8 @@ def get_loss(typ, fac=1.0, **kwargs):
             importance = tf.exp(-kwargs.get("neighbor_scale", 1.0) *
                                 kw.get("num_fluid_neighbors"))
             diff = (tf.reduce_sum(
-                ((target - prev) - (pred - inp))**2, axis=-1) +
-                    1e-9)**kwargs.get("gamma", 0.5)
+                ((target - prev) - (pred - inp)) ** 2, axis=-1) +
+                    1e-9) ** kwargs.get("gamma", 0.5)
             return fac * tf.reduce_mean(importance * diff)
 
         return f
@@ -106,6 +109,8 @@ def get_loss(typ, fac=1.0, **kwargs):
         return partial(emd_loss, **kargs)
     elif typ == "hist":
         return compare_dist
+    elif typ == "dense_pbf":
+        return partial(density_loss_pbf, fac=fac, **kwargs)
     else:
         raise NotImplementedError()
 
@@ -128,7 +133,7 @@ def _grid_pos(pos, res):
                                  tf.int32),
                          tf.ones((tf.shape(pos)[0], 1), tf.bool),
                          shape=[xs, ys, zs, 1])
-    mask = tf.reshape(mask, (-1, ))
+    mask = tf.reshape(mask, (-1,))
 
     return grid_pos[mask]
 
@@ -138,7 +143,7 @@ def grid_pos(pos, voxel_size, centralize=False, pad=0, hyst=0.1):
         center = tf.reduce_mean(pos, axis=0)
         pos = pos - center
 
-    #discretize
+    # discretize
     dpos = tf.concat([
         tf.cast(tf.floor(pos / tf.maximum(voxel_size, 1e-5) -
                          tf.where(voxel_size >= 1e-5, hyst, tf.constant(0.0))),
@@ -147,7 +152,7 @@ def grid_pos(pos, voxel_size, centralize=False, pad=0, hyst=0.1):
                          tf.where(voxel_size >= 1e-5, hyst, tf.constant(0.0))),
                 dtype=tf.int32)
     ],
-                     axis=0)
+        axis=0)
     offset = tf.stack(tf.meshgrid(tf.where(voxel_size[0] >= 1e-5,
                                            tf.range(-pad, 2 + pad),
                                            tf.range(0, 1)),
@@ -187,10 +192,10 @@ def grid_pos_bnds(pos, voxel_size, centralize=False):
         maxpos = tf.maximum(tf.reduce_max(pos, axis=0) - minpos, 1e-7)
         r = tf.round(maxpos / tf.maximum(voxel_size, 1e-5))
 
-        #discretize
+        # discretize
         dpos = tf.cast(tf.round((pos - minpos) / maxpos * r), dtype=tf.int32)
     else:
-        #discretize
+        # discretize
         dpos = tf.cast(tf.round(pos / tf.maximum(voxel_size, 1e-5)),
                        dtype=tf.int32)
 
@@ -302,8 +307,8 @@ def compute_density(out_pos, in_pos=None, radius=0.005, win=None):
         row_splits=neighbors_row_splits)
 
     dist = neighbors - tf.expand_dims(out_pos, axis=1)
-    #dist = tf.expand_dims(out_pos, axis=0) - tf.expand_dims(out_pos, axis=1)
-    dist = tf.reduce_sum(dist**2, axis=-1) / radius**2
+    # dist = tf.expand_dims(out_pos, axis=0) - tf.expand_dims(out_pos, axis=1)
+    dist = tf.reduce_sum(dist ** 2, axis=-1) / radius ** 2
     dens = tf.reduce_sum(win(dist), axis=-1)
     return dens
 
@@ -317,7 +322,7 @@ def quat_mult(q, r):
         r[..., 0] * q[..., 3] - r[..., 1] * q[..., 2] + r[..., 2] * q[..., 1] +
         r[..., 3] * q[..., 0]
     ],
-                    axis=-1)
+        axis=-1)
 
 
 def quat_conj(q):
@@ -357,7 +362,7 @@ def compute_transformed_dx(pos, scale=None, rot=None, radius=0.005):
         scale = tf.RaggedTensor.from_row_splits(
             values=tf.gather(scale, neighbors_index),
             row_splits=neighbors_row_splits)
-        #scale = (neighbors + tf.expand_dims(scale, axis=1)) / 2
+        # scale = (neighbors + tf.expand_dims(scale, axis=1)) / 2
         dx = dx * scale
 
     dx = tf.reduce_mean(dx, axis=1)
@@ -373,7 +378,7 @@ def compute_pressure(out_pts,
     if inp_pts is None:
         inp_pts = out_pts
     dens = compute_density(out_pts, inp_pts, win=win) if dens is None else dens
-    pres = tf.keras.activations.relu(stiffness * ((dens / rest_dens)**7 - 1))
+    pres = tf.keras.activations.relu(stiffness * ((dens / rest_dens) ** 7 - 1))
     return pres
 
 
@@ -396,6 +401,14 @@ def density_loss(gt,
 
     err = tf.keras.activations.relu(pred_dens - rest_dens - eps)
     return tf.reduce_mean(err)
+
+
+def density_loss_pbf(label, pos, pred_dens, density0, fac, eps=0.01, use_max=False, **kwargs):
+    rest_dens = density0
+    if use_max:
+        return tf.abs(tf.reduce_max(pred_dens) - rest_dens) / rest_dens
+    err = tf.keras.activations.relu(pred_dens - rest_dens - eps)
+    return tf.reduce_mean(err) * fac
 
 
 def emd_loss(y_true, y_pred, n=None, m=None):
